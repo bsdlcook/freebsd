8a3056cfd35e1fe682faf72af6fd8d40b3d58b65
syne <syne@wired.sh>
2018-06-23 09:45:37 +0100

    Updated theme manager configs and theme script.

--- .scripts/theme.sh
+++ .scripts/theme.sh
@@ -6,7 +6,7 @@ background=$(grep "theme" ~/.suckless/themes/theme.h | cut -b 35-43 | sed s/\"//
 foreground=$(grep "theme" ~/.suckless/themes/theme.h | cut -b 46-54 | sed s/\"//g)
 convert ~/.scripts/.bsd.png -fuzz 20% -trim +repage -fill $accent -opaque "#333333" ~/.scripts/.bsd-icon.png
 convert -size 2560x1080 canvas:$background -gravity center ~/.scripts/.bsd-icon.png -composite ~/.scripts/.background.png
-sed -i '' s/$(cat ~/.config/zathura/zathurarc | grep "highlight-color" | awk '{print $3}' | sed s/\"//g)/$accent/ ~/.config/zathura/zathurarc
-sed -i '' s/$(cat ~/.config/zathura/zathurarc | grep "recolor-lightcolor" | awk '{print $3}' | sed s/\"//g)/$background/ ~/.config/zathura/zathurarc
-sed -i '' s/$(cat ~/.config/zathura/zathurarc | grep "statusbar-fg" | awk '{print $3}' | sed s/\"//g)/$foreground/ ~/.config/zathura/zathurarc
+sed -i '' s/$(cat ~/.config/zathura/zathurarc | grep "highlight-color" | awk '{print $3}' | sed s/\"//g)/$accent/g ~/.config/zathura/zathurarc
+sed -i '' s/$(cat ~/.config/zathura/zathurarc | grep "recolor-lightcolor" | awk '{print $3}' | sed s/\"//g)/$background/g ~/.config/zathura/zathurarc
+sed -i '' s/$(cat ~/.config/zathura/zathurarc | grep "statusbar-fg" | awk '{print $3}' | sed s/\"//g)/$foreground/g ~/.config/zathura/zathurarc
 ~/.scripts/background.sh
