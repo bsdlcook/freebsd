8aa401d9519b5f88268110ba580dc44df29586cc
syne <syne@wired.sh>
2019-01-22 02:05:45 +0000

    Update post install script

--- post-install.sh
+++ post-install.sh
@@ -1,72 +1,124 @@
 #!/usr/bin/env bash
-if [ $(id -u) -eq 0 ]; then
-	echo "This script shouldn't be run as root, please use a non-root user" && exit
-fi
 
-if [ -z $(pkg info -x security/doas 2>/dev/null | head -n 1) ]; then
-	echo 'Please install doas with "pkg install security/doas" with the root account, and add yourself with the command: echo "permit nopass YOUR_USER as root" | tee -a /usr/local/etc/doas.conf' && exit
-fi
+declare -a PACKAGES=(
+	shells/fish
+	editors/neovim
+	misc/vifm
+	graphics/ImageMagick7 
+	graphics/feh
+	graphics/zathura
+	graphics/zathura-pdf-mupdf
+	audio/cmus
+	sysutils/tmux 
+	sysutils/htop 
+	sysutils/neofetch
+	x11-fonts/nerd-fonts
+	x11/xorg
+	x11/xinit
+	x11/libX11
+	x11/libXau
+	x11/libxcb
+	x11/libXdmcp
+	x11/libXext
+	x11/libXinerama
+	devel/dbus
+)
 
-declare -a packages=(editors/neovim ftp/wget graphics/ImageMagick graphics/feh graphics/zathura graphics/zathura-pdf-mupdf audio/cmus sysutils/tmux sysutils/htop sysutils/py-ranger sysutils/neofetch x11-fonts/terminus-font x11-fonts/nerd-fonts x11/dmenu x11/xorg x11/libX11 x11/libXau x11/libxcb x11/libXdmcp x11/libXext x11/libXinerama devel/libpthread-stubs devel/cmake)
+banner() {
+cat << "EOF"
+	       ,        ,
+              /(        )`
+              \ \___   / |
+              /- _  `-/  '
+             (/\/ \ \   /\		
+             / /   | `    \	
+             O O   ) /    |
+             `-^--'`<     '
+            (_.)  _  )   /
+             `.___/`    /
+               `-----' /
+  <----.     __ / __   \
+  <----|====O)))==) \) /====
+  <----'    `--' `.__,' \
+               |        |    lewis' freebsd post-installation setup script.
+                \       /	   ver 1.
+           ______( (_  / \______
+         ,'  ,-----'   |        \     - gitlab.com/love_lace/freebsd-setup
+         `--{__________)        \/    
+EOF
+}
 
-echo "Checking packages..."
-for package in ${packages[*]}; do
-	echo -ne "[?] $package: "
-	if [ -z $(pkg info -x $package 2>/dev/null | head -n 1) ]; then
-		echo -ne "Not installed\n"
-		doas pkg install -y $package
-		#fish -c "pinstall $package --source" # install from source
-	else 
-		echo -ne "Installed\n"
+check_root() {
+	printf "\nchecking perms... "
+	[ $(id -u) -eq 0 ] && printf "error: please re-run this script as a non-privileged user.\n" && exit 1
+	printf "passed.\n"
+}
+
+check_doas() {
+	printf "checking doas... "
+	[ -z $(pkg info -x security/doas 2>/dev/null | head -n1) ] && \
+		printf "error: please install doas by typing: 'pkg install security/doas' with a privileged user; after add your user account with the command: echo 'permit nopass your_user as root' | tee -s /usr/local/etc/doas.conf - then re-run the script.\n" && exit  1
+	printf "passed.\n"
+}
+
+install_packages() {
+	printf "\nchecking packages...\n"
+	for PACKAGE in ${PACKAGES[*]}; do
+		printf "[#] $PACKAGE: "
+		if [ -z $(pkg info -x $PACKAGE 2>/dev/null | head -n1) ]; then
+			printf "not installed. "
+			doas pkg install -y $PACKAGE &>/dev/null
+			printf "passed.\n"
+		else
+			printf "passed.\n"
+		fi
+	done
+}
+
+set_shell() {
+	printf "\nchanging default shell...\n"
+	chsh -s $(which fish)
+}
+
+check_dein() {
+	printf "\nchecking dain... "
+	if [ ! -d ~/.cache/dein ]; then
+		printf "not installed. downloading setup script. "
+		fetch -qo - https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh | bash -s ~/.cache/dein &>/dev/null
+		printf "passed.\n"
+	else
+		printf "passed.\n"
 	fi
-done
+}
 
-echo -e "\nChecking for dein..."
-echo -ne "[?] dein: "
-if [ ! -d ~/.cache/dein ]; then
-	echo -ne "Not installed\nDownloading setup script...\n"
-	curl -fsSL https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh | bash -s ~/.cache/dein &>/dev/null
-	echo -ne "dein now installed\n"
-else
-	echo -ne "Installed\n"
-fi
+dbus_setup() {
+	printf "\nsetting up dbus...\nmachine id: "
+	dbus-uuidgen | doas tee /etc/machine-id
+	doas sysrc dbus_enable="YES"
+	doas sysrc hald_enable="YES"
+	printf "dbus setup complete.\n"
+}
 
-echo -e "\nChecking for fish..."
-echo -ne "[?] fish: "
-if [ -z $(which fish) ]; then
-        echo -ne "Not installed\nDownloading (please wait)..."
-        git clone --quiet https://github.com/fish-shell/fish-shell && cd fish-shell
-        mkdir build && cd "$_" && cmake -DCMAKE_BUILD_TYPE=Release ..
-        doas make install
-        cd ../.. && rm -rf ./fish-shell
-        echo -ne "Adding to /etc/shells\n"
-        which fish | doas tee -a /etc/shells
-        echo -ne "Changing default shell (enter password)\n"
-        chsh -s $(which fish)
-        echo -ne "fish now installed\n"
-else
-	echo -ne "Installed\n"
-fi
+copy_configs() {
+	printf "\ncopying configuration files... "
+	cp -fR {.config,.scripts,.suckless,.xinitrc,.tmux.conf} ~ &>/dev/null 	
+	printf "passed.\n"
+}
 
-echo -e "\nChecking for IBM Plex fonts..."
-echo -ne "[?] IBM Plex: "
-if [ -z "$(fc-list | grep -i ibmplex)" ]; then
-	echo -ne "Not installed\nDownloading (please wait)..."
-	git clone --quiet https://github.com/IBM/plex/ && find ./plex/ -name *Mono*.ttf -exec doas mv {} /usr/local/share/fonts/TTF/ \; && rm -rf ./plex/
-	echo -ne "\nRebuilding font cache...\n"
-	fc-cache -fv
-else
-	echo -ne "Installed\n"
-fi
+compile_suckless() {
+	printf "\ncompiling suckless software... "
+	doas make install clean -C ~/.suckless/dwm &>/dev/null
+	doas make install clean -C ~/.suckless/st  &>/dev/null
+	doas make install clean -C ~/.suckless/dmenu &>/dev/null
+	printf "passed.\n\nsetup complete. please reboot your system.\n\n-lewis"
+}
 
-echo -e "\nCopying files to $HOME..."
-\cp -fR . ~ &>/dev/null
-\rm -rf ~/.{git,gitignore} ~/screenshots/ ~/setup.sh ~/README.md &>/dev/null
-
-echo -e "\nSetting up dbus uuid..."
-dbus-uuidgen | doas tee /etc/machine-id
-
-echo -e "\nCompiling software (dwm,st)..."
-doas make install clean -C ~/.suckless/dwm
-doas make install clean -C ~/.suckless/st
-echo -e "\n\nSetup complete! Please reboot your system."
+banner
+check_root
+check_doas
+install_packages
+set_shell
+check_dein
+dbus_setup
+copy_configs
+compile_suckless
