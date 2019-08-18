639230c4041b778c0d24a3dd892e6082d001e344
vulcan <vulcan@wired.sh>
2019-08-18 17:33:00 +0100

    Update fish config enviroment variables.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -23,8 +23,11 @@ alias sysf="sys-fetch"
 
 set -x fish_greeting
 set -x LC_ALL en_GB.UTF-8
+set -x LC_CTYPE en_GB.UTF-8
 set -x PATH $PATH ~/.scripts/
 set -x BROWSER firefox
 set -x EDITOR emacs
-set -x VISUAL emacs
+set -x VISUAL emacs 
 set -x TERM xterm-256color
+set -x GOROOT /usr/local/go
+set -x GOPATH ~/Development/.go
