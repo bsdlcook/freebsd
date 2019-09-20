45c229227c9ebdc27e19888c7344e7985eccddb4
Lewis Cook <vulcan@wired.sh>
2019-09-20 22:07:26 +0100

    Add default wallpaper and typefix on dmenu config.

--- .suckless/app/dmenu/config.h
+++ .suckless/app/dmenu/config.h
@@ -2,7 +2,7 @@
 #include "../theme.h"
 
 /* general appearance */
-static int topbar                  = _topbar; 	     /* -b  option; if 0, dmenu appears at bottom                                  */
+static int topbar                  = top_bar; 	     /* -b  option; if 0, dmenu appears at bottom                                  */
 static const char *fonts[]         = { theme.font }; /* -fn option overrides fonts[0]; the x11 menu font                           */
 static const char *prompt          = NULL;           /* -p  option; prompt to the left of input field                              */
 static unsigned int lines          = 0;              /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
